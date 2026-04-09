cc2e0263be34322ebf8dc6c7362f57d85c516d0e
Shankara <shankarbabu926@gmail.com>
2026-04-10 01:09:44 +0530

    Added terraform modules for azure

---
+++ modules/resourcegroup/variable.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  description = "Name of the Azure resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where the resource group will be created"
+  type        = string
+}
